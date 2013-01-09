57fc39ad20b22452ae702bb9d4fd1fd8207fcc86
Iztok Jeras <iztok.jeras@gmail.com>
2013-01-09 16:47:13 +0100

    fixed test of Gray/binary conversion functions

--- run_test_gray.sh
+++ run_test_gray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sources="gray_tb.out tbn/gray_tb.v"
+sources="tbn/gray_tb.v"
 
 # cleanup first
 rm -f gray_tb.out
@@ -8,7 +8,7 @@ rm -f gray_tb.out
 for cdc_ff in "2" "3" "4" "5" 
 do
   # compile Verilog sources (testbench and RTL) with Icarus Verilog
-  iverilog -o -DCDC_FF=$cdc_ff
+  iverilog -o gray_tb.out -DCDC_FF=$cdc_ff $sources
 
   # run the simulation
   vvp gray_tb.out 
